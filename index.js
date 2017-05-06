870863d68cb2cba7d516f2c09ba9fb7997d820ed
DESKTOP-Q7ED8MI\THUAN <pht2707@gmail.com>
2017-05-06 20:38:57 +0700

    add handle bar view

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 var express = require("express");
 var app = express();
+var exphbs  = require('express-handlebars');
 
 app.use(express.static("public"));
 app.use(express.static("bower_components"));
 
-app.get("/", function(req, res){
-  res.send("Hello express js!!");
+app.engine('hbs', exphbs({}));
+app.set('view engine', 'hbs');
+
+app.get('/home', function (req, res) {
+    res.render('index', {title: 'HBS index page', message: 'Hello homepage'});
+});
+
+app.get('/albums', function (req, res) {
+    var albums = [
+      {
+        link: "phanthuan.html",
+        image: "image/PhanThuan/1.jpg",
+        name: "Phan Hữu Thuận",
+        pictures: 5,
+        views: 38
+      },
+      {
+        link: "thanhtram.html",
+        image: "image/ThanhTram/2.jpg",
+        name: "Trần Ngọc Thanh Trâm",
+        pictures: 5,
+        views: 208
+      }
+      //["phanthuan.html", "image/PhanThuan/1.jpg", "Phan Hữu Thuận", 5, 38],
+      //["thanhtram.html", "image/ThanhTram/2.jpg", "Trần Ngọc Thanh Trâm", 5, 108]
+    ]
+    res.render('albums', {albums: albums});
+});
+
+app.get('/about', function (req, res) {
+    res.render('about', {});
+});
+
+app.get('/blog', function (req, res) {
+    res.render('blog', {});
 });
 
 app.listen(2707);
